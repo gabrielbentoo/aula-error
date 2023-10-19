1b572fb4a5dd0425e3d702e216044d44031ad9c5
Aline Takai <99260756+alineathomaz@users.noreply.github.com>
2023-10-19 16:46:09 -0300

    Update main.js

--- main.js
+++ main.js
@@ -35,20 +35,18 @@ function draw()
 { 
     image(video, 0, 0, 600, 500);   
    
-      fill("#FF0000");
-      stroke("#FF0000");
+    fill("#FF0000");
+    stroke("#FF0000");
 
-      if(scoreLeftWrist > 0.2)
-      {
-
-      
+    if(scoreLeftWrist > 0.2)
+    {
       circle(leftWristX,leftWristY,20);
       InNumberleftWristY = Number(leftWristY);
       remove_decimals = floor(InNumberleftWristY);
       volume = remove_decimals/500;
       document.getElementById("volume").innerHTML = "volume =" + volume;
       song.setVolume(volume);
-      }
+    }
 }
 
 function play()
@@ -56,7 +54,6 @@ function play()
     song.play();
     song.setVolume(1);
     song.rate(1);
-
 }
 
 function gotPoses(results)
@@ -64,8 +61,8 @@ function gotPoses(results)
     if(results.length >0);
     {
         console.log(results);
-scoreLeftWrist = results[0].pose.keypoints[9].score;
-console.log("scoreLeftWrist =" + scoreLeftWrist);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist =" + scoreLeftWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
